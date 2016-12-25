e1ecc8f74536d46235910ae3f5c1e8c82497a76f
Taku <gcobs135102@gmail.com>
2016-12-26 01:41:21 +0800

    done basic Stores

--- app/Router/Index.jsx
+++ app/Router/Index.jsx
@@ -5,11 +5,27 @@ import Stores from '../Stores/Stores';
 class Index extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { selectedStore: [] };
     }
 
     selectStore(data, isSelected) {
-        console.log(data, isSelected);
+        let selectedStore = this.state.selectedStore;
+        if (isSelected) {
+            selectedStore.push(data);
+            this.setState({ selectedStore });
+        }
+        else {
+            let key = -1;
+            for (let i = 0; i < selectedStore.length; i++) {
+                if (selectedStore[i].id == data.id) {
+                    key = i;
+                    break;
+                }
+            }
+            selectedStore.splice(key, 1)
+            this.setState({ selectedStore });
+        }
+        console.log(this.state.selectedStore);
     }
 
     render() {

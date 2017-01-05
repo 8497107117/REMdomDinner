755866419888ee34327a277837af7dfa473eb783
Taku <gcobs135102@gmail.com>
2017-01-05 16:44:59 +0800

    props auth

--- app/Stores/Store.jsx
+++ app/Stores/Store.jsx
@@ -12,12 +12,17 @@ class Store extends React.Component {
     }
 
     render() {
+        let type = this.props.data.storetype ? <div>{this.props.data.storetype.type}</div> : <div>無</div>;
+        let area = this.props.data.area ? <div>{this.props.data.area.area}</div> : <div>無</div>;
         return (
-            <div className="store" onClick={this.selectStore.bind(this)}>
+            <div className="store">
                 <div>{this.props.data.name}</div>
                 <div>{this.props.data.address}</div>
                 <div>{this.props.data.phone}</div>
                 <div>{this.props.data.avg_price}</div>
+                {type}
+                {area}
+                <button onClick={this.selectStore.bind(this)}>想吃</button>
             </div>
         );
     }

7d7d728f56eafe20cb38fed04d6e5b7d4c6e0248
Taku <gcobs135102@gmail.com>
2017-01-06 20:14:38 +0800

    change store config

--- app/Stores/Store.jsx
+++ app/Stores/Store.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Store extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { isOpen: false };
     }
 
     selectStore(event) {
@@ -11,17 +11,39 @@ class Store extends React.Component {
         this.props.selectStore(this.props.data);
     }
 
+    showStoreInfo() {
+        let isOpen = !this.state.isOpen;
+        this.setState({ isOpen });
+    }
+
     render() {
-        let type = this.props.data.storetype ? <div>{this.props.data.storetype.type}</div> : <div>無</div>;
-        let area = this.props.data.area ? <div>{this.props.data.area.area}</div> : <div>無</div>;
+        let storeInfo;
+        let type = this.props.data.type ? <div>{this.props.data.type__name}</div> : <div>無</div>;
+        let area = this.props.data.area ? <div>{this.props.data.area__name}</div> : <div>無</div>;
+        if (this.state.isOpen) {
+            storeInfo = (
+                <div>
+                    <button onClick={this.showStoreInfo.bind(this)}>關閉</button>
+                    <div>{this.props.data.name}</div>
+                    <div>{this.props.data.address}</div>
+                    <div>{this.props.data.phone}</div>
+                    <div>{this.props.data.avg_price}</div>
+                    {type}
+                    {area}
+                </div>
+            );
+        }
         return (
-            <div className="store" onClick={this.selectStore.bind(this)}>
-                <div>{this.props.data.name}</div>
-                <div>{this.props.data.address}</div>
-                <div>{this.props.data.phone}</div>
-                <div>{this.props.data.avg_price}</div>
-                {type}
-                {area}
+            <div className="store">
+                <div>
+                    <div>{this.props.data.name}</div>
+                    <img src={this.props.data.url} alt={this.props.data.name} />
+                </div>
+                <div>
+                    <button onClick={this.selectStore.bind(this)}>想吃</button>
+                    <button onClick={this.showStoreInfo.bind(this)}>商家資訊</button>
+                </div>
+                {storeInfo}
             </div>
         );
     }
